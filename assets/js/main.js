a14bf8f965d3e0f2314ead93abfd71853cb828b5
saiyaka2000 <saiyakamondal1@gmail.com>
2022-08-30 09:06:25 +0530

    seo website
    Made with Boostrap5

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+const menubar = document.querySelector('.hamburger-menu');
+const menuList = document.querySelector('.menu_list');
+const navbar = document.querySelector('.navbar');
+menubar.onclick = ()=>{
+    menubar.classList.toggle("active");
+	menuList.classList.toggle("active");
+
+}
+
+
+$('.testimo_slider').slick({
+  dots: true,
+  infinite: false,
+  autoplay: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-chevron-left"></i></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-chevron-right"></i></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+	
+
